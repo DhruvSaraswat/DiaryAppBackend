f0f0fb9a41b8583af43c7d26e328f1dbdd643795
Dhruv Saraswat <saraswat.dhruv@gmail.com>
2024-03-11 01:57:05 +0530

    This commit removes NoRecordFoundException and all its occurrences

--- src/main/java/com/example/diaryappbackend/diaryappbackend/api/GlobalExceptionHandler.java
+++ src/main/java/com/example/diaryappbackend/diaryappbackend/api/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.example.diaryappbackend.diaryappbackend.api;
 import com.example.diaryappbackend.diaryappbackend.model.error.DataDeletionException;
 import com.example.diaryappbackend.diaryappbackend.model.error.DataPersistenceException;
 import com.example.diaryappbackend.diaryappbackend.model.error.ErrorResponse;
-import com.example.diaryappbackend.diaryappbackend.model.error.NoRecordFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,15 +11,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-    @ExceptionHandler(NoRecordFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ResponseBody
-    public ErrorResponse handleNoRecordFoundException(NoRecordFoundException exception) {
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setMessage("No Record Found");
-        return errorResponse;
-    }
 
     @ExceptionHandler(DataPersistenceException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
--- src/main/java/com/example/diaryappbackend/diaryappbackend/model/error/NoRecordFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.diaryappbackend.diaryappbackend.model.error;
-
-public class NoRecordFoundException extends RuntimeException {
-    public NoRecordFoundException() {
-        super();
-    }
-}
